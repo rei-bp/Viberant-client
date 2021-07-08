3a332816bfc696e98898dde75f56e3f27bb136e5
Consecratio <jcasique@gmail.com>
2021-07-08 10:17:48 -0700

    stubbed out routes and default components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+
 import './App.css';
+import Navbar from './components/Navbar'
+import Login from './components/Login'
+import Register from './components/Register'
+import Profile from './components/Profile'
+import Welcome from './components/Welcome'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <header>
+        <Navbar />
       </header>
-    </div>
+      <div className="App">
+        <Switch>
+
+          <Route
+            exact path="/"
+            component={ Welcome }
+          />
+
+          <Route
+            path="/register"
+            render={ props => <Register {...props} /> }
+          />
+
+          <Route
+            path="/login"
+            render={ props => <Login {...props} /> }
+          />
+
+          {/* eventually do a conditional render here */}
+          <Route
+            path="/profile"
+            render={ props => <Profile {...props} /> }
+          />
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
