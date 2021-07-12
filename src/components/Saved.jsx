e2c45a1ea82bc65a462ab39ea77951bd1ca003e0
die-perez <pzandie@gmail.com>
2021-07-12 14:35:05 -0700

    added joined/saved components

---
+++ src/components/Saved.jsx
@@ -0,0 +1,11 @@
+const Saved = () => {
+    return (
+        <div>
+            <h2>Thing to do</h2>
+            <h6>dates about thing</h6>
+            <p>content</p>
+        </div>
+    )
+}
+
+export default Saved
