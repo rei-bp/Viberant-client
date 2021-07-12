3d679b49e32b0faf26a6cc17b8e759a704ba234b
BeckyBoo88 <rbooco07@gmail.com>
2021-07-12 16:31:43 -0600

    added tags page

---
+++ src/components/Tags.jsx
@@ -0,0 +1,14 @@
+const Tags = () => {
+// call into POST DB, pull specific tags to display on page
+
+    return(
+        <ul>
+            <li>Will</li>
+            <li>Be</li>
+            <li>Links</li>
+            <li>To</li>
+            <li>Specific</li>
+            <li>Tags</li>
+        </ul>
+    )
+}
