7dcee09d27dcaca98414353f427725728a1dfd81
BeckyBoo88 <rbooco07@gmail.com>
2021-07-12 14:40:47 -0600

    added post route

---
+++ src/components/Posts.jsx
@@ -0,0 +1,16 @@
+const Posts = () => {
+// map through the POST DB to display all posts with specific tags
+
+    return (
+        <div>
+            I will display all the posts
+            Title,
+            Date,
+            Content
+
+            tags? 
+        </div>
+    )
+}
+
+export default Posts
