5575b22d8ef2a82b82b21efc0f1af4448e627b9f
die-perez <pzandie@gmail.com>
2021-07-12 21:26:20 -0700

    bootstrap cards

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,14 +2,14 @@ import Posts from './Posts'
 import Search from './Search'
 
 
-
-
 const Welcome = () => {
 
 
     return (
         <div>
             <Search />
+            <Posts />
+            <Posts />
             <Posts />
         </div>
     )
