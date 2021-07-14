4aeaeb2fe1e49e929ba4f5d330bd985c402e5cbd
BeckyBoo88 <rbooco07@gmail.com>
2021-07-14 16:31:37 -0600

    newPost added function useStates and onChange

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,13 +1,44 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Modal, Form, Button } from 'react-bootstrap'
 import { useState } from 'react'
+import axios from 'axios'
 
 
-const NewPost = () => {
+const NewPost = (props) => {
     const [show, setShow] = useState(false);
+    const [user_id, setUser_id] = useState('')
+    const [title, setTitle] = useState('')
+    const [tags, setTags] = useState('')
+    const [content, setContent] = useState('')
+    const [maxAttendees, setMaxAttendees] = useState('')
+    const [eventDate, setEventDate] = useState('')
+    const [address, setAddress] = useState('')
+    const [imgUrl, setImgUrl] = useState('')
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    console.log(props)
+
+    const submitPost = async (e) => {
+        try {
+            e.preventDefault()
+            const requestBody = {
+                title: title,
+                tags: tags,
+                content: content,
+                max_attendees: maxAttendees,
+                event_date: eventDate,
+                address: address,
+                img_url: imgUrl
+            }
+            const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/api-v1/posts/new`, requestBody)
+            console.log('🦄',response)
+            
+        } catch (error) {
+            console.log('🆘', error)
+        }
+    }
 
     return (
         <div>
@@ -20,50 +51,44 @@ const NewPost = () => {
                 <Modal.Title>Create A New Post</Modal.Title>
                 </Modal.Header>
                 <Modal.Body> 
-                <Form>
+                <Form onSubmit={submitPost}>
                     <Form.Group className="mb-3" controlId="formBasicId">
-                        <Form.Label>User Id</Form.Label>
-                        <Form.Control type="text" placeholder="Enter user id" />
+                        <Form.Control type="hidden" placeholder="Enter user id" />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicTitle">
                         <Form.Label>Event Title</Form.Label>
-                        <Form.Control type="text" placeholder="Enter event title" />
+                        <Form.Control type="text" placeholder="Enter event title" onChange={e => setTitle(e.target.value)} value={title} />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicTags">
                         <Form.Label>Tags</Form.Label>
-                        <Form.Control type="text" placeholder="Enter tags here" />
+                        <Form.Control type="text" placeholder="Enter tags here" onChange={e => setTags(e.target.value)} value={tags} />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicContent">
                         <Form.Label>Add event description here</Form.Label>
-                        <Form.Control type="text" placeholder="Event description" />
+                        <Form.Control type="text" placeholder="Event description" onChange={e => setContent(e.target.value)} value={content} />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicMaxAttendees">
                         <Form.Label>Max Attendees</Form.Label>
-                        <Form.Control type="number" placeholder="Max number of attendees" />
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" controlId="formBasicAttendees">
-                        <Form.Label>Current Number of Attendees</Form.Label>
-                        <Form.Control type="number" placeholder="Current number of attendees" />
+                        <Form.Control type="number" placeholder="Max number of attendees" onChange={e => setMaxAttendees(e.target.value)} value={maxAttendees} />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicDate">
                         <Form.Label>Date of the Event</Form.Label>
-                        <Form.Control type="date" placeholder="Enter date" />
+                        <Form.Control type="date" placeholder="Enter date" onChange={e => setEventDate(e.target.value)} value={eventDate} />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicAddress">
                         <Form.Label>Event Location</Form.Label>
-                        <Form.Control type="text" placeholder="Event location" />
+                        <Form.Control type="text" placeholder="Event location" onChange={e => setAddress(e.target.value)} value={address} />
                     </Form.Group>
 
                     <Form.Group controlId="formFile" className="mb-3">
                         <Form.Label>Event photo</Form.Label>
-                        <Form.Control type="file" />
+                        <Form.Control type="file" onChange={e => setImgUrl(e.target.value)} value={imgUrl} />
                     </Form.Group>
                 
                 </Form>
